1d12e723d8971b518a54a11eda75dfc82c601e87
sbx_user1051 <sbx_user1051@169.254.8.173>
2023-09-12 20:11:01 +0000

    Tue Sep 12 20:11:01 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,6 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    print(f"SKUs in basket: {skus}")
+    
     raise NotImplementedError()
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,4 +3,4 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_checkout(self):
-        assert hello_solution.hello('Adam') == 'Hello, Adam!'
+        assert checkout_solution.checkout('ABCDE') == 'Hello, Adam!'
