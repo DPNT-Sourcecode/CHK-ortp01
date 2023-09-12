50fbaf380035945303644697daa7e7e49bf3aad0
sbx_user1051 <sbx_user1051@169.254.8.173>
2023-09-12 20:26:20 +0000

    Tue Sep 12 20:26:20 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,15 +11,21 @@ SKUS = {
 }
 
 def checkout(skus):
-    if skus == "": 
-        total = -1
-    elif isinstance(skus[0], int):
-        if skus[0] == SKUS.get('offer_qty'):
-            total = SKUS.get('offer_price', -1)
-    else:
-        total = SKUS.get('price', -1)
+
+    total = 0:
+
+    for sku in skus:
+
+        if skus == "": 
+            total = -1
+        elif isinstance(skus[0], int):
+            if skus[0] == SKUS[sku].get('offer_qty'):
+                total += SKUS[sku].get('offer_price', -1)
+        else:
+            total += SKUS[sku].get('price', -1)
 
     return total
+
 
 
 
