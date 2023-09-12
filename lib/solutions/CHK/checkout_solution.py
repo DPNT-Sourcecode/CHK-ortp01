0367947ce5c27a086ec8dec8f94a045f9f512833
sbx_user1051 <sbx_user1051@169.254.8.173>
2023-09-12 20:19:00 +0000

    Tue Sep 12 20:19:00 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,12 +2,25 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+SKUS = {
+    "A": {"price" : 50, "offer_price" : 130, "offer_qty": 3},
+    "B": {"price" : 30, "offer_price" : 45, "offer_qty": 2},
+    "C": {"price" : 20},
+    "D": {"price" : 15}
+}
+
 def checkout(skus):
+
+
     if skus == "": 
         return -1
+    if isinstance(skus[0], int):
+
     else:
         print(f"SKUs in basket: {skus}")
 
     
     raise NotImplementedError()
+
 
